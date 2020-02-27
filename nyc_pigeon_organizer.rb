797e1e8f815a2b5c22377c4b987e147327481e83
Dakota Willingham <kota@vaga.tk>
2020-02-27 22:41:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   pigeon_names = data[:gender][:male].concat(data[:gender][:female])
   counter = 0
   while counter < pigeon_names.length do
-    pigeon_list[pigeon_names[counter].to_symbol] = {}
+    pigeon_list[pigeon_names[counter].to_sym] = {}
     binding.pry
   end
   # data[:gender].each do |type|
