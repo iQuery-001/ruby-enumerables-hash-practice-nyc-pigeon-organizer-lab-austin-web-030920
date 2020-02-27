746048d1288140e29be280bac1586479e5096a65
Dakota Willingham <kota@vaga.tk>
2020-02-27 23:12:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
     counter += 1
   end
   pigeon_list.each do |(key, value)|
-    key.to_string
+    key.to_s
   end
   data.each do |(type, option)|
     option.each do |(option, value)|
