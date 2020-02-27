42d9591f0f6c9988afa5879cfb74fd5853e05d6f
Dakota Willingham <kota@vaga.tk>
2020-02-27 23:26:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,9 +16,8 @@ def nyc_pigeon_organizer(data)
     option.each do |(option, value)|
       value.each do |name|
         pigeon_list[name][type] << option.to_s
-        binding.pry
       end
     end
   end
-
+  binding.pry
 end
