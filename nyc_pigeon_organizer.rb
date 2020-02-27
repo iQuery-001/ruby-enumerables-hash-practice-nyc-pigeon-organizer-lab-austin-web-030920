8666cf4314c5efeed6bc52e3bfb89bea29d6b872
Dakota Willingham <kota@vaga.tk>
2020-02-27 22:27:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,14 +2,18 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   pidgeon_list = {}
-  data.each_pair do |(type, option)|
-    value.each do |(option, value)|
-      binding.pry
-      count = 0
-      while count < value.length do
-        pidgeon_list[:value[count]] = {}
-      end
-    end
+  counter = 0
+  data[:gender].each do |n|
+    binding.pry
+  end
+  # data.each_pair do |(type, option)|
+  #   value.each do |(option, value)|
+  #     binding.pry
+  #     count = 0
+  #     while count < value.length do
+  #       pidgeon_list[:value[count]] = {}
+  #     end
+  #   end
     # binding.pry
     # if !pidgeon_list[value]
   end
