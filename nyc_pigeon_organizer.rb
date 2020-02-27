623367237d3cb633fc718910cdc00142d305eec6
Dakota Willingham <kota@vaga.tk>
2020-02-27 22:36:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,18 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pidgeon_list = {}
-  data[:gender].each do |type|
-    type.each do |option|
-      counter = 0
-      while counter < option.length
-        pidgeon_list[option[counter]] = {}
-        binding.pry
-        counter += 1
-      end
-    end
-  end
+  pigeon_list = {}
+  pigeon_names = data[:gender][:male].concat(data[:gender][:female])
+  # data[:gender].each do |type|
+  #   type.each do |option|
+  #     counter = 0
+  #     while counter < option.length
+  #       pidgeon_list[option[counter]] = {}
+  #       binding.pry
+  #       counter += 1
+  #     end
+  #   end
+  # end
   # data.each_pair do |(type, option)|
   #   value.each do |(option, value)|
   #     binding.pry
